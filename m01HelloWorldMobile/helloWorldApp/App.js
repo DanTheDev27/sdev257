b4d68d9f0c1e933ebaf6985128db203bcd567ab9
Danny Contreras <dannycontreras@Dannys-MacBook-Pro.local>
2025-03-22 16:46:28 -0400

    Added comment

--- m01HelloWorldMobile/helloWorldApp/App.js
+++ m01HelloWorldMobile/helloWorldApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>"Hi, my name is Danny Contreras, and I am a mobile developer".</Text>
+      <Text>"Hi, my name is Danny Contreras, and I am a mobile developer".</Text> {/* Hello World message that is part of the requirements for the assignment */}
       <StatusBar style="auto" />
     </View>
   );

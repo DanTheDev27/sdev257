1c26f0f137e65c3560b5e44483f59edef6c8d53a
Danny Contreras <dannycontreras@Dannys-MacBook-Pro.local>
2025-04-12 11:53:33 -0400

    Used Geo-location to add my location and other points of interest

---
+++ m04GeoLocation/geoLocation/App.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { View, StatusBar } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import styles from "./styles";
+
+StatusBar.setBarStyle("dark-content");
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation.coords);
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapView}
+        showsPointsOfInterest={true}
+        showsUserLocation={false}
+        followUserLocation={true}
+        region={
+          location
+            ? {
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+              }
+            : undefined
+        }
+      >
+        <Marker
+          title="Ivy Tech Sellersburg"
+          description="Home Campus"
+          coordinate={{
+            latitude: 38.389755,
+            longitude: -85.762614,
+          }}
+        />
+        <Marker
+          title="Home"
+          description="The neighbourhood"
+          coordinate={{
+            latitude: 38.49347,
+            longitude: -85.77741,
+          }}
+        />
+        <Marker 
+          title="Dodger Stadium"
+          description="Dodger Stadium"
+          coordinate={{
+            latitude: 34.0739,
+            longitude: -118.2400,
+          }}
+        />
+        <Marker 
+        title="My hometown"
+        description="Kaiser Permanente"
+        coordinate={{
+          latitude: 37.336401,
+          longitude: -121.999142,
+        }}
+        />
+        <Marker 
+        title="Cracker Barrel"
+        description="Good pancakes"
+        coordinate={{
+          latitude: 38.394681,
+          longitude: -85.759741,
+        }}
+        />
+      </MapView>
+    </View>
+  );
+}
